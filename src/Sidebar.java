d12b918aab56f6e564e8ce7fe44252ae9710a18f
Carter Johnson <90220938+johnson-carter@users.noreply.github.com>
2025-07-28 16:16:45 -0500

    Configured Window Properties & Project Structure

---
+++ src/Sidebar.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import assets.Theme;
+
+
+public class Sidebar extends JFrame{
+    /* Sidebar is the UI component that provides UI functionality
+     * 
+     */
+    public Sidebar() {
+        setWindowProperties();
+        initializeSidebarContent();
+        setVisible(true);
+    }
+
+    private void initializeSidebarContent() { 
+        JPanel sidebarPanel = new JPanel();
+        sidebarPanel.setBackground(Theme.BACKGROUND_COLOR);
+        sidebarPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        add(sidebarPanel);
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            System.exit(0);
+        });
+        sidebarPanel.add(exitButton);
+    }
+
+    private void setWindowProperties() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int x = screenSize.width - 300;
+        int y = 0; // Top of the screen. 
+        System.out.println("Setting sidebar position to: " + x + ", " + y);
+
+        setLocation(x, 10);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, screenSize.height - 60); // Set width to 300px and height to full screen heightJF
+        setUndecorated(true);
+    }
+}
---
+++ src/assets/Theme.java
@@ -0,0 +1,10 @@
+package assets;
+
+import java.awt.Color;
+
+public class Theme {
+    public static final Color BACKGROUND_COLOR = new Color(40,40,40);
+    public static final Color TEXT_COLOR = new Color(50, 50, 50);
+    public static final Color HIGHLIGHT_COLOR = new Color(100, 150, 250);
+
+}   
